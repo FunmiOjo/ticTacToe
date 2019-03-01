912eaad80017b198c15b253f34718aa2c31d2c54
Funmi Ojo <oluwafunmi.ojo@gmail.com>
2019-03-01 17:58:22 -0500

    Added test for click behavior on cells

--- client/tests/Cell.spec.js
+++ client/tests/Cell.spec.js
@@ -1,17 +1,30 @@
 import { expect } from 'chai'
 import { shallow } from 'enzyme'
-import { sinon } from 'sinon'
+import sinon from 'sinon'
 import React from 'react'
 import { Cell } from '../components/Cell'
 
 describe('Cell component', () => {
   let wrapper
+  const mockMarkCell = sinon.spy()
+  const mockSwitchPlayer = sinon.spy()
   beforeEach('set up wrapper', () => {
-    wrapper = shallow(<Cell />)
+    wrapper = shallow(
+      <Cell markCell={mockMarkCell} switchPlayer={mockSwitchPlayer} />
+    )
   })
 
   it('displays a game token', () => {
     const text = wrapper.text()
-    expect(text).to.be.oneOf(['', 'X', 'O'])
+    expect(text).to.be.oneOf(['-', 'X', 'O'])
+  })
+
+  it('handles clicks properly', () => {
+    const div = wrapper.find('div')
+    const wrapperInstance = wrapper.instance()
+    wrapperInstance.handleClick = sinon.spy()
+    div.simulate('click')
+    expect(mockMarkCell.called).to.equal(true)
+    expect(mockSwitchPlayer.called).to.equal(true)
   })
 })
